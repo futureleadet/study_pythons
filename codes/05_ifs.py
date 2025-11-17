6f40687a7b1a6f02bc6037c3ffb2657ad9342faf
futureleadet <futureleadet@gmail.com>
2025-11-17 18:02:33 +0900

    first commit

---
+++ codes/05_ifs.py
@@ -0,0 +1,15 @@
+first = 2
+
+# if ifs >= 3 :
+#     print("babo babo")
+#     print("it's me!")
+
+second = 6
+
+first = int(input("input first number: "))
+second = int(input("input second number: "))
+if first >= second :
+    print("babo")
+    print("it's me!")
+
+print("End od program.")
