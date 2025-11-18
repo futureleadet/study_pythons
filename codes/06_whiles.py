c9273c6f5c0c54b516a167f62100cf6a8bdb74d9
futureleadet <futureleadet@gmail.com>
2025-11-18 12:58:10 +0900

    ..

--- codes/06_whiles.py
+++ codes/06_whiles.py
@@ -1,6 +1,10 @@
 first = 5
 
 while first > 0 :
-    print("while 값 : "+first)
+    print("while 값 : ",first)
+    if first == 3 :
+        print("continue 실행")
+        break
     first = first - 1
-    
+
+print("while 종료")
