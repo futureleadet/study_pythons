160fa890c6e0ad2a1af95257661bfa0edfef8269
futureleadet <futureleadet@gmail.com>
2025-11-18 17:31:29 +0900

    ..

--- codes/05_ifs.py
+++ codes/05_ifs.py
@@ -1,16 +1,34 @@
+# if elif else
+
 first = 2
 
 # if ifs >= 3 :
 #     print("babo babo")
 #     print("it's me!")
 
-second = 6
+# second = 6
 
-first = int(input("input first number: "))
-second = int(input("input second number: "))
+# first = int(input("input first number: "))
+# second = int(input("input second number: "))
 
-if first >= second :
-    print("babo")
-    print("it's me!")
+# if first >= second :
+#     print("babo")
+#     print("it's me!")
 
-print("End od program.")
+# print("End od program.")
+
+age = int(input("Enter your age: "))
+if age < 4 :
+    print("Your ticket is free!")
+elif age < 18 :
+    print("Your ticket is $5.")
+else :
+    print("Your ticket is $10.")
+
+print("End of program.")
+
+pass 
+fourth = "welcome to python!"
+fourth_word = "python"
+if fourth_word in fourth :
+    print("yes, 'python' is here!")
