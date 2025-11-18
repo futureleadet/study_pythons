6548a52b665792ce00b7638b9176c05325458144
futureleadet <futureleadet@gmail.com>
2025-11-18 16:37:30 +0900

    ..

---
+++ codes/06_ifs_whiles_fors.py
@@ -0,0 +1,30 @@
+
+# second = "Programming"
+# first = f"Welcome to Python Strings {second}"
+
+# print(first)
+
+
+# first = 5
+
+# while first > 0:
+#     print(first)
+#     first = first - 1
+
+# kor = [70, 80, 90, 40, 50]
+# eng = [90, 80, 70, 70, 60]
+# sum_all = 0
+
+# for k in kor:
+#     sum_all = sum_all + k     # 오류 위치
+# print(f"국어 점수 합계: {sum_all}")
+
+
+# kor = [70, 80, 90, 40, 50]
+# eng = [90, 80, 70, 70, 60]
+
+# sum_total = 0
+
+# for i in range(len(kor)):    # 오류 발생
+#     sum_total = sum_total + kor[i] + eng[i]
+# print(f"국어와 영어 총합: {sum_total}")
