1b1138b49204c4e2bfd9a7e40042c592bbfaf1d7
futureleadet <futureleadet@gmail.com>
2025-11-19 16:07:07 +0900

    ..

---
+++ codes/08_functions_parameters.py
@@ -0,0 +1,28 @@
+# 함수 사용
+# def funtion_name(param_first,...) :
+    #실행할 코드
+    #return reture_value
+
+kor = 60
+eng = 70
+math = 80
+
+# sum = kor + eng
+
+def get_sum(korean, english, mathmatics=0) :
+    summation = korean + english + mathmatics
+    return summation
+
+# sum = get_sum(kor, eng, math)
+# print(f"총점 : {sum}")
+
+# sum = get_sum(kor, eng)
+# print(f"총점 : {sum}")
+
+kor_score = [90, 80, 70, 60, 50]
+eng_score = [85, 75, 65, 55, 45]
+math_score = [88, 78, 68, 58, 48]
+
+lenth = len(kor_score)
+len_list = range(lenth)
+pass
