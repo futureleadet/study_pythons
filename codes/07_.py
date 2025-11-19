f44005198cf9adb864d48c80d58d17ebbc572f63
futureleadet <futureleadet@gmail.com>
2025-11-19 09:33:04 +0900

    ..

--- codes/07_.py
+++
@@ -1,9 +0,0 @@
-# 중급 난이도 문제 1 — 문자열과 f-string 활용
-# second 문자열에 "Python"이 포함되어 있는지 확인하고,
-#  "Welcome!"과 합쳐서 출력하는 코드를 작성하시오.
-
-fourth = "Welcome! Python is fun"
-second = "Python"
-
-if second in fourth :
-    print(f"Welcome! + {second}")
---
+++ codes/07_ifs_whiles_fors.py
@@ -0,0 +1,35 @@
+
+
+# second = "Python is fun"
+# second_word = "Python"
+
+# if second_word in second :
+#     print(f"Welcome! {second}")
+
+# first = 5
+
+# while first > 0 :
+#     print("while 값 : "+str(first))
+#     if first == 2 :
+#         print("special")
+#     first = first - 1
+############################ 실패 #########################    
+# kor = [70, 80, 90, 40, 50]
+# eng = [90, 80, 70, 70, 60]
+# total_scores = 0
+
+# for i in range(len(kor)):
+#     total_scores = total_scores + kor[i] + eng[i]
+# print(f"각 학생 점수의 총합: {total_scores}")
+############################# 실패 #########################
+
+
+# kor = [70, 80, 90, 40, 50]
+# total = 0
+
+# for score in kor:
+#     if score >= 60:
+#         total = total + score
+
+# print(f"60점 이상 합계 = : {total}")
+
