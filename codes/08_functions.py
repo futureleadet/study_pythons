e43bcdae58bd6a6bf0bafb8463b9b1012b51415c
futureleadet <futureleadet@gmail.com>
2025-11-19 13:38:54 +0900

    ..

---
+++ codes/08_functions.py
@@ -0,0 +1,30 @@
+# temp1 = 77
+# celsius1 = (temp1 - 32) * 5 / 9
+# print(celsius1)
+
+# temp2 = 95
+# celsius2 = (temp2 - 32) * 5 / 9
+# print(celsius2)
+
+# temp3 = 50
+# celsius3 = (temp3 - 32) * 5 / 9
+# print(celsius3)
+
+# 함수 사용
+# def funtion_name(param_first,...) :
+    #실행할 코드
+
+def to_celsius(temp) :
+    celsius = (temp -3) * 5 / 9
+    return celsius
+
+pass
+to_celsius(120)
+temp1 = to_celsius(77)
+print(temp1)
+
+temp2 = 120
+result2 = to_celsius(temp2)
+print(result2)
+pass
+
